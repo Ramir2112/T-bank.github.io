fb28ecce3c61bf8950cb173a9d8811ab40494e90
Ramir2112 <98506871+Ramir2112@users.noreply.github.com>
2024-09-14 12:52:49 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.getElementById('tgBtn').addEventListener('click', function() {
+  // Replace with your Telegram Web App URL
+  const tgUrl = 'https://telegram.org/dl/webogram';
+  window.open(tgUrl, '_blank');
+});
